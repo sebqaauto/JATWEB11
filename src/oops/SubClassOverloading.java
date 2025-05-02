83459e96a418072d3b8e5e963a2c40d90756717f
Sebastian Selvaraj Augustine <sebastianselvarajaugustine@Sebastians-MacBook-Pro.local>
2025-05-02 17:10:00 +0530

    third commit

--- src/oops/SubClassOverloading.java
+++ src/oops/SubClassOverloading.java
@@ -9,6 +9,31 @@ public class SubClassOverloading extends OverloadingTest{
 	public String add(String a, String b) {
 		return a+b;
 	}
+	
+	public String input = "Malayalam";
+	
+	public void findPalindrome() {
+		input = input.toLowerCase();
+		char [] arr = input.toCharArray();
+		char [] revArr = new char[arr.length];
+		int counter = 0;
+		for(int i =arr.length-1; i>=0; i-- ) {
+			revArr[counter] = arr[i];
+			counter++;
+		}
+		
+		for (char c : revArr) {
+			System.out.print(c);
+		}
+		for (char c : arr) {
+			System.out.print(c);
+		}
+		
+		if(arr.equals(revArr)) {
+			System.out.println("It is a palindrome");
+		}
+	}
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -16,6 +41,7 @@ public class SubClassOverloading extends OverloadingTest{
 		SubClassOverloading child = new SubClassOverloading();
 		System.out.println(child.add(4.5f, 5.6f, 6.5f));
 		System.out.println(child.add("Mon", "day"));
+		child.findPalindrome();
 		
 	}
 
