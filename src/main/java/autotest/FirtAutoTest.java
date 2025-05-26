4f735c4170c6e31cd6d0a92f1fc971309d407211
Sebastian Selvaraj Augustine <sebastianselvarajaugustine@Sebastians-MacBook-Pro.local>
2025-05-26 10:08:50 +0100

    Eight commit

---
+++ src/main/java/autotest/FirtAutoTest.java
@@ -0,0 +1,23 @@
+package autotest;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class FirtAutoTest {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		// Before Selenium Manager - the driver files should be downloaded and the path of it should be set
+		ChromeDriver driver = new ChromeDriver();
+		//FirefoxDriver driver2 = new FirefoxDriver();
+		//driver.get("https://www.facebook.com/");
+		driver.get("https://demowebshop.tricentis.com/");
+		driver.findElement(By.className("ico-register")).click();
+		driver.findElement(By.className("ico-login")).click();
+		Thread.sleep(3000);
+		driver.findElement(By.linkText("Register")).click();
+		
+	}
+
+}
